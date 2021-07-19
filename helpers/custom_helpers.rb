3715d5a14b1cc776942bec53b27d0b0c851450ea
Fernando Guillen <fguillen.mail@gmail.com>
2021-07-19 11:26:39 +0200

    Project partial reading from json data

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -7,7 +7,12 @@ module CustomHelpers
   end
 
   def project_detail(name, value)
-    capitalized = name.split('_').map{ |e| e.capitalize }.join(" ")
-    "<li class=\"list-group-item\"><strong>#{capitalized}:</strong> #{value}</li>"
+    capitalized = name.split('_').map(&:capitalize).join(" ")
+
+    if value =~ /^http/
+      "<li class=\"list-group-item\"><strong>#{capitalized}:</strong> <a href=\"#{value}\">#{value}</a></li>"
+    else
+      "<li class=\"list-group-item\"><strong>#{capitalized}:</strong> #{value}</li>"
+    end
   end
 end
